984888571d92f9ff23a95968eb6cbace16710980
JoJunYeong <you@example.com>
2020-06-11 21:14:13 +0900

    create_idpw 완료 메인작업시작

--- app/src/main/java/kr/co/wero/www/sksk/createidpw.kt
+++ app/src/main/java/kr/co/wero/www/sksk/createidpw.kt
@@ -3,6 +3,7 @@ package kr.co.wero.www.sksk
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_createidpw.*
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -14,6 +15,50 @@ class createidpw : AppCompatActivity() {
         back.setOnClickListener {
             finish()
         }
+
+        next.setOnClickListener {
+
+            back.visibility = View.GONE        // 버튼 안보이게 하기
+            next.visibility = View.GONE
+            previous.visibility = View.VISIBLE      // 버튼 보이게 하기
+            done.visibility = View.VISIBLE
+
+
+            create_first_layout.visibility = View.GONE     // 레이아웃 안보이게 하기
+            create_second_layout.visibility = View.VISIBLE      // 레이아웃 보이게 하기
+
+
+
+        }
+
+        previous.setOnClickListener {
+
+            back.visibility = View.VISIBLE        // 버튼 보이게 하기
+            next.visibility = View.VISIBLE
+            previous.visibility = View.GONE      // 버튼 안보이게 하기
+            done.visibility = View.GONE
+
+
+            create_first_layout.visibility = View.VISIBLE        // 레이아웃 보이게 하기
+            create_second_layout.visibility = View.GONE            // 레이아웃 안보이게 하기
+
+
+
+        }
+
+
+
+        done.setOnClickListener {
+
+           finish()
+
+
+
+        }
+
+
+
+
 
 
     }
