3328d239e6a922d36ac270b3875a18bc40ea20af
JoJunYeong <you@example.com>
2020-06-19 16:36:23 +0900

    심박수까지 완벽스하게 뜸

--- app/src/main/java/kr/co/wero/www/sksk/mainpage.kt
+++ app/src/main/java/kr/co/wero/www/sksk/mainpage.kt
@@ -1,24 +1,25 @@
 package kr.co.wero.www.sksk
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
-import android.view.KeyEvent
-import android.widget.Toast
-import androidx.viewpager.widget.ViewPager
-import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.activity_mainpage.*
 import android.app.ProgressDialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import android.content.Context
-import android.os.AsyncTask
-import java.util.*
 import android.bluetooth.BluetoothSocket
+import android.content.Context
+import android.content.Intent
+import android.os.AsyncTask
+import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_mainpage.*
 import java.io.IOException
-
+import java.io.InputStream
+import java.util.*
 
 
 class mainpage : AppCompatActivity() {
@@ -26,7 +27,12 @@ class mainpage : AppCompatActivity() {
     private var mFlag = false
     private val mHandler: Handler = Handler()
 
+
+
+
     companion object {
+        private var mTxtReceive: TextView? = null
+        private var mReadThread: ReadInput? = null
         var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
         var m_bluetoothSocket: BluetoothSocket? = null
         lateinit var m_progress: ProgressDialog
@@ -34,7 +40,12 @@ class mainpage : AppCompatActivity() {
         var m_isConnected: Boolean = false
         lateinit var m_address: String
         lateinit var m_name: String
+
+
+
+
     }
+
 
 
 
@@ -45,14 +56,19 @@ class mainpage : AppCompatActivity() {
 
         m_address = intent.getStringExtra(BluetoothSearch2.EXTRA_NAME)
 
+        ConnectToDevice(this).execute()
 
-/*
+        mTxtReceive = findViewById<View>(R.id.heartbeat) as TextView
+
+
         profile_btn.setOnClickListener {
             /*val settings= Intent(applicationContext,settings::class.java)
             startActivity(settings)*/
             val intent=Intent(applicationContext,MyProfile::class.java)
             startActivity(intent)
         }
+
+
 
         var list=ArrayList<Int>()
         list.add(1)
@@ -81,7 +97,7 @@ class mainpage : AppCompatActivity() {
 
 
 
-*/
+
     }
 
 
@@ -107,6 +123,127 @@ class mainpage : AppCompatActivity() {
             }
         }
         else return super.onKeyDown(keyCode, event)
+    }
+
+    private fun sendCommand(input: String) {
+        if (m_bluetoothSocket != null) {
+            try{
+                m_bluetoothSocket!!.outputStream.write(input.toByteArray())
+            } catch(e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun disconnect() {
+        if (m_bluetoothSocket != null) {
+            try {
+                m_bluetoothSocket!!.close()
+                m_bluetoothSocket = null
+                m_isConnected = false
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        finish()
+    }
+
+    private class ConnectToDevice(c: Context) : AsyncTask<Void, Void, String>() {
+        private var connectSuccess: Boolean = true
+        private val context: Context
+
+        init {
+            this.context = c
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            m_progress = ProgressDialog.show(context, "Connecting...", "please wait")
+        }
+
+        override fun doInBackground(vararg p0: Void?): String? {
+            try {
+                if (m_bluetoothSocket == null || !m_isConnected) {
+                    m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+                    val device: BluetoothDevice = m_bluetoothAdapter.getRemoteDevice(m_address)
+                    m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_myUUID)
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
+                    m_bluetoothSocket!!.connect()
+                }
+            } catch (e: IOException) {
+                connectSuccess = false
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+            if (!connectSuccess) {
+                Log.i("data", "couldn't connect")
+            } else {
+
+                m_isConnected = true
+                mReadThread = ReadInput()
+            }
+            m_progress.dismiss()
+        }
+    }
+
+    private class ReadInput : Runnable {
+        private var bStop = false
+        private val t: Thread
+
+
+
+        val isRunning: Boolean
+            get() = t.isAlive
+
+        override fun run() {
+            val inputStream: InputStream
+
+            try {
+                inputStream = m_bluetoothSocket!!.inputStream
+                while (!bStop) {
+                    val buffer = ByteArray(256)
+                    if (inputStream.available() > 0) {
+                        inputStream.read(buffer)
+
+                        val strInput = String(buffer, 0, 4)
+
+                        /*
+                         * If checked then receive text, better design would probably be to stop thread if unchecked and free resources, but this is a quick fix
+                         */
+                        mTxtReceive?.post(Runnable {
+                            mTxtReceive!!.text = strInput
+                            /*
+                                                    int txtLength = mTxtReceive.getEditableText().length();
+                                                    if(txtLength > mMaxChars){
+                                                        mTxtReceive.getEditableText().delete(0, txtLength - mMaxChars);
+                                                    }
+                    */
+                        })
+                    }
+                    Thread.sleep(10)
+                }
+            } catch (e: IOException) {
+// TODO Auto-generated catch block
+                e.printStackTrace()
+            } catch (e: InterruptedException) {
+// TODO Auto-generated catch block
+                e.printStackTrace()
+            }
+        }
+
+        fun stop() {
+            bStop = true
+        }
+
+        init {
+            t = Thread(this, "Input Thread")
+            t.start()
+        }
     }
 
 
