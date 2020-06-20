1e15401ca9ca40cd83d1b15b335cc7035e74d02d
OhRyungyi <giligoni120515@gmail.com>
2020-06-20 16:41:33 +0900

    사진 스와이프 제외하고는 다 구현

--- app/src/main/java/kr/co/wero/www/sksk/like_final.kt
+++ app/src/main/java/kr/co/wero/www/sksk/like_final.kt
@@ -3,7 +3,6 @@ package kr.co.wero.www.sksk
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_chating_final.*
 import kotlinx.android.synthetic.main.activity_like_final.*
 import kotlinx.android.synthetic.main.activity_like_final.btn_back
 import kotlinx.android.synthetic.main.activity_like_final.detail
